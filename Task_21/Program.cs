e2da62ab357679f175ff35cc02c4945a7a068385
«Kwork» <kwork@yandex.ru>
2022-03-23 18:27:10 +0300

    commit folder21

---
+++ Task_21/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+// Задача 23: Напишите программу, которая принимает на вход число (N)
+// и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.WriteLine("введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <=N; i++)
+{
+    Console.WriteLine($"куб числа {i} = {i*i*i}");
+}
